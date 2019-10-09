27ac4bfd01fdb43a126844765850a8b9de41c388
IvanAndreiDaniel <56355504+IvanAndreiDaniel@users.noreply.github.com>
2019-10-09 21:03:05 +0200

    Add files via upload

--- produttore.c
+++ produttore.c
@@ -5,46 +5,35 @@
 #include <unistd.h>
 #define MAX_STRING_LENGTH 256
 
-//codice controllato :)
-// produttore.c NON e' un filtro
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[]) {
     int fd, written, righe;
-    char *file_out;
-    char riga[MAX_STRING_LENGTH], buf[MAX_STRING_LENGTH];
+    char *file_out, riga[MAX_STRING_LENGTH], buf[MAX_STRING_LENGTH];
     
     //controllo numero argomenti
-    if (argc != 2){ 
+    if (argc != 2) { 
         perror(" numero di argomenti sbagliato"); 
         exit(1);
     } 
     
-    file_out = argv[1]; 
+    file_out = argv[1];
 
     fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
-    
-    if (fd < 0){
+    if (fd < 0) {
         perror("P0: Impossibile creare/aprire il file");
         exit(2);
     }
     
-    printf("Inserisci le nuove righe, o EOF [CTRL^D] per terminare\n");
+    printf("Inserisci le nuove righe, o EOF [CTRL^D] per terminare.\n");
     
-
     //fgetc(stdin)!= EOF 
-    while (fgets(riga, MAX_STRING_LENGTH, stdin)){
-
+    while (fgets(riga, MAX_STRING_LENGTH, stdin)) {
         written = write(fd, riga, strlen(riga)*sizeof(char));
-        if (written < 0){
+        if (written < 0) {
             perror("P0: errore nella scrittura sul file");
             exit(3);
         }
-        
-
     }
-    //chiudo il file su cui ho scritto
-    close(fd);
-
-    return EXIT_SUCCESS;
     
- }
+    close(fd);
+    return EXIT_SUCCESS;
+}
